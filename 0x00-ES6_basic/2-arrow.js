4b839a8426a8f70f1cd82a46c3a2437f7bfcd09e
Mr-Ndi <onlythenotes@gmail.com>
2024-06-27 02:00:01 +0200

    Making a push or a change

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
 export default () => {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
-    const self = this;
     this.addNeighborhood = function add(newNeighborhood) {
       this.sanFranciscoNeighborhoods.push(newNeighborhood);
       return this.sanFranciscoNeighborhoods;
