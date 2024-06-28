1961f366749caf5cb8560e52930f1f2fb1cef7e8
Mr-Ndi <onlythenotes@gmail.com>
2024-06-29 01:12:14 +0200

    Making a push or a change

--- 0x00-ES6_basic/10-main.js
+++ 0x00-ES6_basic/10-main.js
@@ -1,3 +1,5 @@
 import appendToEachArrayValue from './10-loops.js';
 
 console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
+
+// Rewrite the function appendToEachArrayValue to use ES6’s for...of operator. And don’t forget that var is not ES6-friendly.
