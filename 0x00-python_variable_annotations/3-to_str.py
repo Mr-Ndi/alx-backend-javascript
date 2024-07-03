1413af3a26cd2421722bb27d26cf93f6d3e36ddd
Mr-Ndi <onlythenotes@gmail.com>
2024-07-04 01:03:26 +0200

    Making a push or a change

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""
+    annotated function
+"""
+
+
+def to_str(n: float) -> str:
+    '''A function that takes a float number and returns its string representation.'''
+    return str(n)
