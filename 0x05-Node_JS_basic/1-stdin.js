4919bdc302e65dd31fc247ae801adce29403baa6
Mr-Ndi <onlythenotes@gmail.com>
2024-08-20 21:19:55 +0200

    documenting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
-// displaying a welcoming message
+/*
+ * displaying a welcoming message
+ * listening to user input
+ * converting input to string ansd trim the space around
+ * handling exit process and displays message
+ */
+
 console.log('Welcome to Holberton School, what is your name?');
-// listening to user input
 process.stdin.on('data', (input) => {
-  // converting input to string ansd trim the space around
   const name = input.toString().trim();
   console.log('Your name is: ' + name);
 });
-// handling exit process and displays message
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
   process.exit();
