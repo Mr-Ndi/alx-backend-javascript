f7d03dace5c6a20c7dec00378567c399f7cb5c84
Mr-Ndi <onlythenotes@gmail.com>
2024-08-20 21:22:03 +0200

    revision

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,12 @@
-/*
- * displaying a welcoming message
- * listening to user input
- * converting input to string ansd trim the space around
- * handling exit process and displays message
- */
-
+/* Using Process stdin
+*/
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  console.log('Your name is: ' + name);
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) process.stdout.write(`Your name is: ${chunk}`);
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
