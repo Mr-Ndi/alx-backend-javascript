409e290547852526a5c1fab2c931b5c045f1f201
Mr-Ndi <onlythenotes@gmail.com>
2024-08-22 22:53:00 +0200

    version 0.3

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,26 +3,26 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
     it('rounding first argument', () => {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1.7, 3), 5);
-        assert.equal(calculateNumber(1.2, 3), 4);
-        assert.equal(calculateNumber(1.8, 3), 6);
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1.7, 3), 5);
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
+        assert.strictEqual(calculateNumber(1.8, 3), 6);
     });
 
     it('Rounding second argument', () => {
-        assert.equal(calculateNumber(6, 1.2), 7);
-        assert.equal(calculateNumber(2, 3.1), 5);
-        assert.equal(calculateNumber(6, 1.3), 7);
-        assert.equal(calculateNumber(3, 1.7), 5);
-        assert.equal(calculateNumber(1, 5.9), 7);
+        assert.strictEqual(calculateNumber(6, 1.2), 7);
+        assert.strictEqual(calculateNumber(2, 3.1), 5);
+        assert.strictEqual(calculateNumber(6, 1.3), 7);
+        assert.strictEqual(calculateNumber(3, 1.7), 5);
+        assert.strictEqual(calculateNumber(1, 5.9), 7);
     });
 
     it('Returning the right answer', () => {
-        assert.equal(calculateNumber(1.4, 0.5), 2);
-        assert.equal(calculateNumber(3, 3.4), 6);
-        assert.equal(calculateNumber(45, 5), 80);
-        assert.equal(calculateNumber(20, 7.3), 27);
-        assert.equal(calculateNumber(0.4, 4.3), 5);
-        assert.equal(calculateNumber(0.9, 6.6), 8);
+        assert.strictEqual(calculateNumber(1.4, 0.5), 2);
+        assert.strictEqual(calculateNumber(3, 3.4), 6);
+        assert.strictEqual(calculateNumber(45, 5), 80);
+        assert.strictEqual(calculateNumber(20, 7.3), 27);
+        assert.strictEqual(calculateNumber(0.4, 4.3), 5);
+        assert.strictEqual(calculateNumber(0.9, 6.6), 8);
     });
 });
