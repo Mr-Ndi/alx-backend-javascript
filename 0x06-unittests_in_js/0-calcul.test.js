454ed3532e24d4d64a3997e4ed121b0ccbb6d06f
Mr-Ndi <onlythenotes@gmail.com>
2024-08-22 23:11:57 +0200

    version 4

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,28 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('rounding first argument', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1.7, 3), 5);
-        assert.strictEqual(calculateNumber(1.2, 3), 4);
-        assert.strictEqual(calculateNumber(1.8, 3), 6);
+    it('Rounding the first argument', () => {
+        assert.equal(calculateNumber(1.00, 1), 2);
+        assert.equal(calculateNumber(1.02, 3), 4);
+        assert.equal(calculateNumber(1.03, 3), 4);
+        assert.equal(calculateNumber(1.3, 3), 4);
+        assert.equal(calculateNumber(1.41, 2), 3);
     });
 
-    it('Rounding second argument', () => {
-        assert.strictEqual(calculateNumber(6, 1.2), 7);
-        assert.strictEqual(calculateNumber(2, 3.1), 5);
-        assert.strictEqual(calculateNumber(6, 1.3), 7);
-        assert.strictEqual(calculateNumber(3, 1.7), 5);
-        assert.strictEqual(calculateNumber(1, 5.9), 7);
+    it('Rounding the second argument', () => {
+        assert.equal(calculateNumber(1, 1.2), 2);
+        assert.equal(calculateNumber(1, 3.1), 4);
+        assert.equal(calculateNumber(2, 3.4), 5);
+        assert.equal(calculateNumber(4, 3.1), 7);
+        assert.equal(calculateNumber(8, 2.9), 11);
     });
-
-    it('Returning the right answer', () => {
-        assert.strictEqual(calculateNumber(1.4, 0.5), 2);
-        assert.strictEqual(calculateNumber(3, 3.4), 6);
-        assert.strictEqual(calculateNumber(45, 5), 80);
-        assert.strictEqual(calculateNumber(20, 7.3), 27);
-        assert.strictEqual(calculateNumber(0.4, 4.3), 5);
-        assert.strictEqual(calculateNumber(0.9, 6.6), 8);
+    it('Returning the right answwer', () => {
+        assert.equal(calculateNumber(1.3, 0), 1);
+        assert.equal(calculateNumber(0, 1.2), 1);
+        assert.equal(calculateNumber(1.3, 1.3), 2);
+        assert.equal(calculateNumber(1.7, 1.2), 3);
+        assert.equal(calculateNumber(1.3, 1.8), 3);
+        assert.equal(calculateNumber(1.6, 1.8), 4);
     });
 });
